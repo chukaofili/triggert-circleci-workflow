004c5351889adab82d5d177982e35a646c9998a0
Chuka Ofili <ofili@microsmart.tk>
2019-04-14 03:34:00 +0100

    Updated Function

--- index.js
+++ index.js
@@ -6,26 +6,26 @@
  * @param {Object} res Cloud Function response context.
  *                     More info: https://expressjs.com/en/api.html#res
  */
+const rp = require('request-promise-native');
 exports.triggerCIWorkflow = async (req, res) => {
   const { action, pull_request: { head: { ref: branch } }, repository: { full_name } } = req.body;
   const vcs_type = 'github';
-  const url = `https://circleci.com/api/v1.1/project/${vcs_type}/${full_name}/build?circle-token=${process.env.CIRCLE_TOKEN}`;
   const options = {
-    method: 'post',
+    method: 'POST',
+    uri: `https://circleci.com/api/v1.1/project/${vcs_type}/${full_name}/build?circle-token=${process.env.CIRCLE_TOKEN}`,
     headers: {
-      "Content-type": "application/json"
+      'content-type': 'application/json'
     },
-    body: {
-      branch
-    }
+    body: { branch },
+    json: true
   }
 
   try {
     if (action !== 'opened') return res.status(200).json({ status: 200, body: 'Build skipped' })
-    const response = await fetch(url, options).then((result) => { return result.json(); });
+    const response = await rp(options);
     return res.status(200).json(response);
   } catch (error) {
-    console.log(error)
+    console.error(error)
     return res.status(500).json(error);
   }
 };
