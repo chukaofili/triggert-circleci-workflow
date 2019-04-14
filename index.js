22b87088e53803f716d217a79e803fd4da78aae6
Chuka Ofili <ofili@microsmart.tk>
2019-04-14 02:45:09 +0100

    Second Commit

--- index.js
+++ index.js
@@ -7,5 +7,24 @@
  *                     More info: https://expressjs.com/en/api.html#res
  */
 exports.triggerCIWorkflow = (req, res) => {
-  res.send(`Trigger Circle CI Workflow!`);
+  const { action, head: { ref: branch }, repository: { full_name } } = req.body;
+  const vcs_type = 'github';
+  const url = `https://circleci.com/api/v1.1/project/${vcs_type}/${full_name}/build?circle-token=${process.env.CIRCLE_TOKEN}`;
+  const options = {
+    method: 'post',
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: {
+      branch
+    }
+  }
+
+  try {
+    if (action !== 'opened') return res.status(200).json({status: 200, body: 'Build skipped'})
+    const response = await fetch(url, options).then((result) => { return result.json(); });
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 };
